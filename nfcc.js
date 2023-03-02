ba409d6531609fe3610a0e845dd8ae815c2df83e
saaid111 <93545412+saaid111@users.noreply.github.com>
2023-03-02 05:22:43 +0000

    Add files via upload

---
+++ nfcc.js
@@ -0,0 +1,41 @@
+async function readTag() {
+    if ("NDEFReader" in window) {
+      const ndef = new NDEFReader();
+      try {
+        await ndef.scan();
+        ndef.onreading = event => {
+          const decoder = new TextDecoder();
+          const targetUrl = "https://Apple.com";
+          for (const record of event.message.records) {
+            if (record.url === targetUrl) {
+              const circle = document.querySelector(".circle"); // Replace ".circle" with the selector for the circle element you want to change
+              circle.style.backgroundColor = "yellow";
+              break; // Stop iterating through records since a match has been found
+            }
+          }
+        }
+      } catch(error) {
+        consoleLog(error);
+      }
+    } else {
+      consoleLog("Web NFC is not supported.");
+
+      let timeoutId;
+
+    function consoleLog(data) {
+    const logElement = document.getElementById('log');
+    logElement.innerHTML += data + '\n';
+
+    if (data === "Web NFC is not supported.") {
+    timeoutId = setTimeout(() => {
+      logElement.innerHTML = "";
+    }, 5000); // 5000ms = 5 seconds
+  } else {
+    clearTimeout(timeoutId);
+  }
+}
+
+    }
+  }
+  
+  
